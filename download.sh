226068d76067a7ee400726935c6ff1a4eac79467
Simon Willison <swillison@gmail.com>
2020-09-16 22:20:09 -0700

    Script to fetch latest files

---
+++ download.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -O https://projects.fivethirtyeight.com/polls-page/president_primary_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/president_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/senate_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/house_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/governor_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/president_approval_polls.csv
+curl -O https://projects.fivethirtyeight.com/polls-page/generic_ballot_polls.csv
